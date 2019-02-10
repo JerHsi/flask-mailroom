61de7db8a0acd18edec6c45280f042582b799ce9
Jerry.H <doridoriae86@gmail.com>
2019-02-10 14:12:05 -0800

    Lesson 1 changes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor 
 
 app = Flask(__name__)
 
@@ -11,10 +11,31 @@ app = Flask(__name__)
 def home():
     return redirect(url_for('all'))
 
+
 @app.route('/donations/')
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
+
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        try:
+            donor = Donor.select().where(Donor.name == request.form['name']).get()
+        except Donor.DoesNotExist:
+            new_donor = Donor(name=request.form['name'])
+            new_donor.save()
+            new_donation = Donation(value=request.form['donation'],
+                                    donor=new_donor)
+            new_donation.save()
+        else:
+            new_donation = Donation(value=request.form['donation'], donor=donor)
+            new_donation.save()
+
+        return redirect(url_for('all'))
+
+    return render_template('create.jinja2')
     
 
 if __name__ == "__main__":
